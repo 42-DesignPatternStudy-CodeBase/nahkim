894842f6a3933c352191709f00693706f5b7462f
nahkim <nahkim@student.42seoul.kr>
2023-02-06 23:06:40 +0900

    :white_check_mark: Singleton Pattern
    싱글톤 패턴

---
+++ SingletonPattern/Singleton.java
@@ -0,0 +1,10 @@
+package SingletonPattern;
+
+public class Singleton {
+  private static class singleInstanceHolder {
+    private static final Singleton INSTANCE = new Singleton();
+  }
+  public static synchronized Singleton getInstance() {
+    return singleInstanceHolder.INSTANCE;
+  }
+}
