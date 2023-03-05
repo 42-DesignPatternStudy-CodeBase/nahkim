6eb74c7e209f986716fd2d0b1e78c9d99464b6dd
nahkim <nahkim@student.42seoul.kr>
2023-03-05 18:10:38 +0900

    :memo: Ing : AdapterPattern
    AdapterPattern 하는 중

---
+++ AdapterPattern/PrintBannerInheritance.java
@@ -0,0 +1,22 @@
+package AdapterPattern;
+
+public class PrintBannerInheritance implements Print {
+  
+  // Todo
+  public PrintBannerInheritance(Banner bannerName) {
+
+  }
+
+  @Override
+  public void printWeak() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'printWeak'");
+  }
+
+  @Override
+  public void printStrong() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'printStrong'");
+  }
+  
+}
